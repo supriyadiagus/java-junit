3c399710aaf45bdda392cbbb5fa7cfe8f4edbc24
supriyadiagus <supriyadi.agus@gmail.com>
2020-07-22 10:30:16 +0000

    added string calcultor kata

--- src/test/java/com/example/project/StringCalculatorTest.java
+++ src/test/java/com/example/project/StringCalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+    
     @Test
     public void itShouldReturn0() {
         StringCalculator stringCalculator = new StringCalculator();
